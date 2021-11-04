ece2e8655ea738a89033ca3e4a21e6526544e088
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-11-04 22:27:54 +0000

    solution

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 #The provided code stub reads an integer n from STDIN. For all non-negative integers from 0 to n, print n*negative
 
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+      print(i*i)
